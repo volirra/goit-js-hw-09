8694e73013137dc4605357513dd9fa17ef0f93ab
volirra <127318805+volirra@users.noreply.github.com>
2023-09-18 15:13:30 +0300

    commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,96 +1,55 @@
 import flatpickr from "flatpickr";
-import 'flatpickr/dist/flatpickr.min.css';
-import { Notify } from "notiflix";
+import "flatpickr/dist/flatpickr.min.css";
+import { convertMs } from "../helpers/02-convert-time";
 
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMin = document.querySelector('[data-minutes]');
+const timerSec = document.querySelector('[data-seconds]');
 const inputEl = document.querySelector('#datetime-picker');
-const btnEl = document.querySelector('button[data-start]');
-const timerDiv = document.querySelector('.timer');
-const daysEl = document.querySelector('span[data-days]');
-const hoursEl = document.querySelector('span[data-hours]');
-const minEl = document.querySelector('span[data-minutes]');
-const secEl = document.querySelector('span[data-seconds]');
+const startEl = document.querySelector('[data-start]');
 
-btnEl.classList.add('disabled');
-let userDate = null;
-
-function convertMs(ms) {
-// Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-// Remaining days
-    const days = pad(Math.floor(ms / day));
-// Remaining hours
-    const hours = pad(Math.floor((ms % day) / hour));
-// Remaining minutes
-    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-// Remaining seconds
-    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-
-return { days, hours, minutes, seconds };
-}
-
-//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-function pad(value) {
-    return String(value).padStart(2, '0');
-}
+startEl.disabled = true;
 
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
+
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
+        // console.log(selectedDates[0]);
+      if (selectedDates[0] < Date.now()) {
+        alert('Please choose a date in the future')
+      } else {
+        startEl.disabled = false;
+      }
+    },
 
-    if(selectedDates[0] < Date.now()) {
-        Notify.failure('Please choose a date in the future');
-            userDate = new Date();
-    } else { 
-        btnEl.disabled = false;
-            btnEl.classList.remove('disabled');
-                userDate = selectedDates[0];
-}
-},
-};
+  };
 
-class Timer  {
-constructor() {
-    this.isActive = false;
-    this.timerId = null;
-    btnEl.disabled = true;
-}
- timerStart() {
-    if (this.isActive) {
-    return;
-}
-this.isActive = true;
-this.timerId = setInterval(()=> {
-    const currentTime = Date.now();
-    const deltaTime = userDate - currentTime;
-    const components = convertMs(deltaTime);
+  startEl.addEventListener('click', onStart);
+    
+  function onStart() {
+    startEl.disabled = true;
+    inputEl.disabled = true;
 
-secEl.textContent = components.seconds;
-minEl.textContent = components.minutes;
-hoursEl.textContent = components.hours;
-daysEl.textContent = components.days;
-    if (deltaTime <= 0) {
-    this.stop();
-    timerDiv.innerHTML = "Time is over!";
-} 
-}, 1000)
+    const IntId = setInterval(() => {
+    const currentDate = Date.now();
+    const selectetDate = new Date(inputEl.value);
+    const difference = selectetDate - currentDate;
 
-}
-    timerStop() {
-        clearInterval(this.timerId);
+    if (difference < 1000) {
+        clearInterval(IntId);
     }
+
+        const timer = convertMs(difference);
+
+        timerDays.textContent = timer.days.toString().padStart(2, 0);
+        timerHours.textContent = timer.hours.toString().padStart(2, 0);
+        timerMin.textContent = timer.minutes.toString().padStart(2, 0);
+        timerSec.textContent = timer.seconds.toString().padStart(2, 0);
+    }, 1000)
 }
 
-    const timer = new Timer();
-    flatpickr(inputEl, options);
-    btnEl.addEventListener('click', () => timer.timerStart());
+flatpickr(inputEl, options);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,8 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
-import { Notify } from "notiflix";
+import throttle  from "lodash.throttle";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formEl = document.querySelector('.form');
 formEl.addEventListener('submit', onFormSubmit);
-
 
 function onFormSubmit(e){
   e.preventDefault();
