34ff074737ba9703653e461899ffe26d389d68b5
volirra <127318805+volirra@users.noreply.github.com>
2023-09-18 18:34:13 +0300

    commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,6 +10,24 @@ const startEl = document.querySelector('[data-start]');
 
 startEl.disabled = true;
 
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = pad(Math.floor(ms / day));
+    // Remaining hours
+    const hours = pad(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+    return { days, hours, minutes, seconds };
+}
 const options = {
     enableTime: true,
     time_24hr: true,
